b6e73bb59e046402965c308532ca599f897fcee4
jrcastle <jrca253@uky.edu>
2023-12-12 17:08:57 -0500

    Build UI

---
+++ CreateGRIDTarget/CreateGRIDTarget.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using CreateGRIDTarget;
+using VMS.TPS.Common.Model.API;
+
+namespace VMS.TPS
+{
+    public class Script
+    {
+        public void Execute(ScriptContext context, Window window)
+        {
+            // Step 1: Get the imaging series UID from context
+            string imageStudyInstanceUID = context.Image.Series.Study.UID;
+            string imageSeriesInstanceUID = context.Image.Series.UID;
+            string patientID = context.Patient.Id;
+            string patientName = context.Patient.Name;
+
+            Dictionary<string, string> seriesInfo = new Dictionary<string, string>();
+            seriesInfo.Add("StudyInstanceUID", imageStudyInstanceUID);
+            seriesInfo.Add("PatientID", patientID);
+            seriesInfo.Add("SeriesInstanceUID", imageSeriesInstanceUID);
+            seriesInfo.Add("PatientName", patientName);
+
+            window.Title = "GRID";
+            window.Height = 650;
+            window.Width = 1200;
+            CreateGRIDTargetUserControl userControl = new CreateGRIDTargetUserControl();
+            window.Content = userControl;
+        }
+    }
+}
---
+++ CreateGRIDTarget/CreateGRIDTargetUserControl.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CreateGRIDTarget
+{
+    ////////////////////////////////////////////////
+    // CreateGRIDTargetUserControl 
+    ////////////////////////////////////////////////
+    public partial class CreateGRIDTargetUserControl : UserControl
+    {
+        ////////////////////////////////////////////////
+        // CONSTRUCTOR
+        ////////////////////////////////////////////////
+        public CreateGRIDTargetUserControl()
+        {
+            InitializeComponent();
+        }
+
+
+        ////////////////////////////////////////////////
+        // GTVSelectionChanged
+        ////////////////////////////////////////////////
+        public void GTVSelectionChanged(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        ////////////////////////////////////////////////
+        // TextBox_GridDiameter_Changed
+        ////////////////////////////////////////////////
+        public void TextBox_GridDiameter_Changed(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        ////////////////////////////////////////////////
+        // TextBox_GridSeparation_Changed
+        ////////////////////////////////////////////////
+        public void TextBox_GridSeparation_Changed(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        ////////////////////////////////////////////////
+        // TextBox_GTVErodeMarginX_Changed
+        ////////////////////////////////////////////////
+        public void TextBox_GTVErodeMarginX_Changed(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        ////////////////////////////////////////////////
+        // TextBox_GTVErodeMarginY_Changed
+        ////////////////////////////////////////////////
+        public void TextBox_GTVErodeMarginY_Changed(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        ////////////////////////////////////////////////
+        // TextBox_GTVErodeMarginZ_Changed
+        ////////////////////////////////////////////////
+        public void TextBox_GTVErodeMarginZ_Changed(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        ////////////////////////////////////////////////
+        // TextBox_GridRotation_Changed
+        ////////////////////////////////////////////////
+        public void TextBox_GridRotation_Changed(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        ////////////////////////////////////////////////
+        // TextBox_GridShiftX_Changed
+        ////////////////////////////////////////////////
+        public void TextBox_GridShiftX_Changed(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        ////////////////////////////////////////////////
+        // TextBox_GridShiftZ_Changed
+        ////////////////////////////////////////////////
+        public void TextBox_GridShiftZ_Changed(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        ////////////////////////////////////////////////
+        // RunButton_Click
+        ////////////////////////////////////////////////
+        public void RunButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        ////////////////////////////////////////////////
+        // AbortButton_Click
+        ////////////////////////////////////////////////
+        public void AbortButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        ////////////////////////////////////////////////
+        // ClearButton_Click
+        ////////////////////////////////////////////////
+        public void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        ////////////////////////////////////////////////
+        // CloseButton_Click
+        ////////////////////////////////////////////////
+        public void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+
+
+    
+}
+
