376862b0a8a3d5ffe365d0897c1ed68784f7409c
jrcastle <jrcastle90@gmail.com>
2024-02-08 15:06:58 -0500

    Update logo

--- CreateGRIDTarget/CreateGRIDTarget.cs
+++ CreateGRIDTarget/CreateGRIDTarget.cs
@@ -4,8 +4,8 @@ using CreateGRIDTarget;
 using System.Reflection;
 using VMS.TPS.Common.Model.API;
 
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.2.1")]
+[assembly: AssemblyFileVersion("1.0.2.1")]
 [assembly: AssemblyInformationalVersion("1.0")]
 [assembly: ESAPIScript(IsWriteable = true)]
 
--- CreateGRIDTarget/Properties/Resources.Designer.cs
+++ CreateGRIDTarget/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace CreateGRIDTarget.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Logo3 {
+            get {
+                object obj = ResourceManager.GetObject("Logo3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
